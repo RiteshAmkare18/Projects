4216110e9b18afa1290097152f75b98047c012bd
SimranAmkare12 <simranamkare@gmail.com>
2026-06-14 11:49:18 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let value = button.innerText;
+
+        if (value === "AC") {
+            expression = "";
+            display.value = "";
+        }
+        else if (value === "DEL") {
+            expression = expression.slice(0, -1);
+            display.value = expression;
+        }
+        else if (value === "=") {
+            try {
+                expression = eval(expression).toString();
+                display.value = expression;
+            } catch {
+                display.value = "Error";
+                expression = "";
+            }
+        }
+        else {
+            expression += value;
+            display.value = expression;
+        }
+    });
+});
